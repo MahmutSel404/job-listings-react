f3a620fd436b5f8ae6c52884314cdb690391b25f
mahmutsel <mahmutsel29@gmail.com>
2020-06-10 23:29:09 +0100

    filter working (functionality works but still needs some css)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JobBoardComponent from "./components/JobBoardComponent";
 import data from "./frontendMentor/data.json";
 import "./App.css";
 
 function App() {
+  const [jobsData, setJobsData] = useState([]);
+  const [filters, setFilters] = useState([]);
+
+  useEffect(() => setJobsData(data), []);
+
+  function filterFunction({ role, level, tools, languages }) {
+    if (filters.length === 0) {
+      return true;
+    }
+    const tags = [role, level];
+    if (tools) {
+      tags.push(...tools);
+    }
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    return tags.some((tag) => filters.includes(tag));
+  }
+  const handleTagClick = (tag) => {
+    setFilters([...filters, tag]);
+  };
+  const filteredJobs = jobsData.filter(filterFunction);
+
   return (
     <div className="App">
       <header className="header-img">
         <img src="./images/bg-header-desktop.svg" alt="" />
       </header>
-      {data.map((job) => (
-        <JobBoardComponent job={job} key={job.id} />
-      ))}
+
+      <div className="job-filter-card">
+        {filters.length > 0 && filters.map((filter) => <span>{filter}</span>)}
+      </div>
+      {filteredJobs.length === 0 ? (
+        <p>Loading....</p>
+      ) : (
+        filteredJobs.map((job) => (
+          <JobBoardComponent
+            job={job}
+            key={job.id}
+            handleTagClick={handleTagClick}
+          />
+        ))
+      )}
     </div>
   );
 }
--- src/components/JobBoardComponent.js
+++ src/components/JobBoardComponent.js
@@ -5,6 +5,8 @@ const JobBoardComponent = ({
     company,
     logo,
     position,
+    isNew,
+    featured,
     role,
     level,
     postedAt,
@@ -13,28 +15,48 @@ const JobBoardComponent = ({
     languages,
     tools,
   },
+  handleTagClick,
 }) => {
+  const tags = [role, level];
+  if (tools) {
+    tags.push(...tools);
+  }
+  if (languages) {
+    tags.push(...languages);
+  }
   return (
-    <div className="job-card">
+    <div className={`job-card ${featured && "job-card-featured"}`}>
       <div>
         <img src={logo} alt={company} />
       </div>
       <div className="job-description">
-        <h3 className="job-company">{company}</h3>
+        <h3 className="job-company">
+          {company}
+          {isNew ? <span className="new">NEW</span> : ""}
+          {featured ? <span className="featured">Featured</span> : ""}
+        </h3>
         <h2 className="job-position">{position}</h2>
         <p className="job-other-details">
           {postedAt}·{contract}·{location}
         </p>
       </div>
-      <div className="lang-and-tools">
-        <span className="lang">{role}</span>
+      <div className="tags">
+        {/* <span className="lang">{role}</span>
         <span className="lang">{level}</span>
         {languages
           ? languages.map((language) => (
               <span className="lang">{language}</span>
             ))
           : ""}
-        {tools ? tools.map((tool) => <span className="lang">{tool}</span>) : ""}
+        {tools ? tools.map((tool) => <span className="lang">{tool}</span>) : ""} */}
+
+        {tags
+          ? tags.map((tag) => (
+              <span onClick={() => handleTagClick(tag)} className="lang">
+                {tag}
+              </span>
+            ))
+          : ""}
       </div>
     </div>
   );
